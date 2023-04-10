dd54f58d901fa27bd35282803263cee27c962139
Arefin Aareef <arefin.aareef@gmail.com>
2023-04-11 00:13:33 +0600

    context api

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Uncle from '../Uncle/Uncle';
 import Father from '../Father/Father';
 import './Grandpa.css'
 
+export const RingContext = createContext('gold')
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
     const ring = 'diamond'
+    const [money, setMoney] = useState(0)
     return (
         <div className='grandpa'>
             <h3>Grandpa</h3>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty ring={ring}></Aunty>
-            </section>
+            <p>Has Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingContext.Provider value='golden ring'>
+                    <section className='flex'>
+                        <Father ring={ring}></Father>
+                        <Uncle></Uncle>
+                        <Aunty ring={ring}></Aunty>
+                    </section>
+                </RingContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
--- src/components/Uncle/Uncle.jsx
+++ src/components/Uncle/Uncle.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousin from '../Cousin/Cousin';
+import { MoneyContext } from '../Grandpa/Grandpa';
 
 const Uncle = () => {
+    const [money, setMoney] = useContext(MoneyContext)
     return (
         <div>
            <h3>Uncle</h3>
+           <p><small>Grandpa Money: {money}</small></p>
+           <button onClick={() => setMoney(money + 1000)}>Send TK</button>
            <section className='flex'>
                 <Cousin>Nabila</Cousin>
                 <Cousin>Kabila</Cousin>
